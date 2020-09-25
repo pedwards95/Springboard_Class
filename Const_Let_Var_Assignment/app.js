bfa8a50f024367a227ff10eef9a4d732917d0230
Paul Edwards <pauldedwards95@gmail.com>
2020-09-25 15:21:40 -0500

    const,let,var exercise

---
+++ Const_Let_Var_Assignment/app.js
@@ -0,0 +1,22 @@
+// 1.
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+/*
+2. What is the difference between var and let?
+3. What is the difference between var and const?
+4. What is the difference between let and const?
+5. What is hoisting?
+*/
+
+// 1
+const PI = 3.14;
+
+//2. Var is function scope and let is block scope. Also, var is stored on the window as a variable.
+// Also var is hoisted.
+
+//3. Var is function scope and const is block scope. Also, var is stored on the window as a variable.
+// Also var is hoisted. Const can not be changed. Const must be given a value at initialization.
+
+//4. Const can not be changed. Const must be given a value at initialization.
+
+//5. Hoisting is when the initialization of variables is done before the running of the rest of the code.
