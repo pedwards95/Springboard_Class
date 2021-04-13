a6f5bea11b3c5cd7bdf1ddff3d9025dcc6a12530
Paul Edwards <pauldedwards95@gmail.com>
2021-04-13 10:28:06 -0500

    async

---
+++ Exercises/31.2_Async-Exercise/number_app.js
@@ -0,0 +1,36 @@
+let num1 = Math.floor(Math.random() * 100);
+let num2 = Math.floor(Math.random() * 100);
+let num3 = Math.floor(Math.random() * 100);
+
+let myFav = Math.floor(Math.random() * 100);
+
+let res = axios.get(`http://numbersapi.com/${num1},${num2},${num3}?json`)
+    .then((res) => {
+        for (num in res.data)
+        {
+            $("#facts").append(`<li>${res.data[num]}</li>`)
+        }
+        
+    })
+
+async function getFavoriteFact(){
+    return await axios.get(`http://numbersapi.com/${myFav}?json`)
+}
+
+getFavoriteFact()
+    .then((res) => {
+        $("#facts").append(`<li>${res.data.text}</li>`)
+        return getFavoriteFact()
+    })
+    .then((res) => {
+        $("#facts").append(`<li>${res.data.text}</li>`)
+        return getFavoriteFact()
+    })
+    .then((res) => {
+        $("#facts").append(`<li>${res.data.text}</li>`)
+        return getFavoriteFact()
+    })
+    .then((res) => {
+        $("#facts").append(`<li>${res.data.text}</li>`)
+    })
+
