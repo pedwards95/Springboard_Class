8a64390f163b278601070f3c8783a4b1aede8836
Paul Edwards <pauldedwards95@gmail.com>
2021-07-06 04:58:55 -0500

    react simple eightball

---
+++ Exercises/39.4-React_EightBall/myapp/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import EightBall from "./EightBall";
+
+function App() {
+  return (
+    <div className="App">
+      <EightBall />
+    </div>
+  );
+}
+
+export default App;
